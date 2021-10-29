cc588ef49abc3850a734ffabf90d90691ee9d283
Kirill Korshunov <kkv2410@gmail.com>
2021-10-29 16:10:19 +0700

    Fix (script.js): done lesson 11

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+'use strict';
 
+let inputText = document.querySelector('#text');
+let inputRange = document.querySelector('#range');
+let firstButton = document.querySelector('#btn');
+let secondButton = document.querySelector('#e_btn');
+let square = document.querySelector('#square');
+let circle = document.querySelector('#circle');
 
+const colorer = function () {
+    square.style.backgroundColor = inputText.value;
+};
 
+const sizer = function (event) {
+    circle.style.height = event.target.value + '%';
+    circle.style.width = event.target.value + '%';
+};
 
-
+firstButton.addEventListener('click', colorer);
+secondButton.style.display = 'none';
+inputRange.addEventListener('input', sizer);
